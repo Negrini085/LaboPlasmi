a26525912eea57db4de6d2c511cd756f81ba6c89
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-03-21 16:17:53 +0100

    Changed file positioning & added imparo.m features

--- MatlabScripts/Imparo/imparo.m
+++ MatlabScripts/Imparo/imparo.m
@@ -10,4 +10,15 @@ clc % Pulisce lo schermo
 % NB: gli indici iniziano da 1, non da zero come di norma
 data = load("cazdata.txt"); % Matrice con 2 colonne
 t = data(:, 1); % Prima colonna sono dei tempi (sintassi stile python, ma senza [])
-x = data(:, 2); % Seconda colonna sono delle posizioni
+v = data(:, 2); % Seconda colonna sono delle velocità
+
+[row col] = size(data); 
+disp(row)
+
+figure(1)
+% Effettuo il plotting di dati (voglio che rimangano visibili duranet script e abbiano griglia)
+plot(t, v, 'k.-'); hold on; grid on % Plotto dati contraddistinti da un punto e collegati da linee
+
+
+xlabel("Time (s)")  % Label asse x
+ylabel("Velocity (m/s)")    % Label asse y
