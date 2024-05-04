b614f8cb6f31ae4b47c1b736cf6790e8bdf7e182
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-04 22:14:40 +0200

    Fatto studio dimensioni della trappola

--- Esperienze/Esperienza 1/Analisi/Configurazione/image_analysis.m
+++ Esperienze/Esperienza 1/Analisi/Configurazione/image_analysis.m
@@ -16,3 +16,28 @@ function noise = rumore_medio(path, n)
     noise = appo;
 end
 
+
+% Abbiamo due immagini che ci consentono di valutare le dimensioni della
+% trappola e di calcolare il fattore di conversione per determinare le
+% dimensioni in metri di un singolo pixel: utilizziamo la parte di codice
+% presente nell'Esperienza 0 per fare il display degli anelli. Abbiamo
+% trovato:
+
+% IMMAGINE 1:
+%        ---> Pixel alto:        408
+%        ---> Pixel basso:      -429
+%        ---> Pixel destro:      417
+%        ---> Pixel sinistro:   -419
+
+% IMMAGINE 2:
+%        ---> Pixel alto:        409
+%        ---> Pixel basso:      -429
+%        ---> Pixel destro:      418
+%        ---> Pixel sinistro:   -419
+
+% Per ottenere delle immagini esattamente centrate traslerò in alto di 10
+% pixel tutte le immagini scattate per questa esperienza: il tasso di
+% convesione è dato da 838 pixel che coprono i 9 cm di diametro della
+% trappola. Presentiamo una lunghezza per pixel in millimetri
+l_pixel = 90/838.0;
+
