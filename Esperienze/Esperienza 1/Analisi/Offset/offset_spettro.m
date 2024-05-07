98e10e90e009f0ce1d85a67df389cf708be4d61f
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-07 18:44:52 +0200

    Risolte problematiche analisi

--- Esperienze/Esperienza 1/Analisi/Offset/offset_spettro.m
+++ Esperienze/Esperienza 1/Analisi/Offset/offset_spettro.m
@@ -2,18 +2,18 @@
 %                Calcolo offset facendo l'analisi in FFT                 %
 %------------------------------------------------------------------------%
 close all
-clear
+clear all
 clc
 
 %sign = [2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 21, 22, 24, 29, 30, 31, 32, 33, 34, 35, 37, 39, 40, 41, 43, 45, 46, 47, 49];
-%sign = [2, 3, 4, 6, 8, 9, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48, 49, 50];
+sign = [2, 3, 4, 6, 8, 9, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48, 49, 50];
 %sign = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 42, 43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 56, 57, 58, 59, 60, 61, 62];
 %sign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 %sign = [1, 2, 3, 6, 7, 8, 9, 10, 11];
 %sign = [1, 2, 3, 4, 6, 7, 8, 10];
 %sign = [1, 2, 4, 5, 6, 7, 8, 9, 10];
 %sign = [1, 3, 4, 5, 6, 8, 9, 10];
-sign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+%sign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 
@@ -22,7 +22,7 @@ function fourierOffset(path, nome_serie, sign, liminf, limsup)
     % File di output
     base = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/Signals/';
     new_data = fopen(['/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Segnali/' nome_serie],'w');
-    fprintf(new_data, '%s \n', 'Omega 1       Omega3     Offset');
+    fprintf(new_data, '%s \n', 'Nu1       Nu3     Offset');
     
 
     % Creo il grafico di controllo per vedere se siamo in una regione ad
@@ -30,9 +30,10 @@ function fourierOffset(path, nome_serie, sign, liminf, limsup)
     data = fopen([base path 'segnale' sprintf('%02i', sign(1)) '.txt'], 'rt');
     N = 3; filescan = textscan(data,'%f %f','HeaderLines',N);
     t = filescan {1,1}; v = filescan {1,2};
+    dt = (t(143) - t(142))*0.001;
 
     % Calcolo la frequenza di campionamento
-    fprintf('Frequenza fondamentale di campionamento: %e\n', 1/(1e-6 * length(t(t<limsup & t>liminf))))
+    fprintf('Frequenza fondamentale di campionamento: %e\n', 1/(dt * length(t(t<limsup & t>liminf))))
 
     figure(1);
     plot(t(t<limsup & t>liminf), v(t<limsup & t>liminf), 'r-', 'LineWidth',2); hold on; grid on;
@@ -49,7 +50,7 @@ function fourierOffset(path, nome_serie, sign, liminf, limsup)
         %---------------------------------------------%
         %        Applico trasformata di Fourier       %
         %---------------------------------------------%
-        dt = 1e-6;
+        dt = (t(143)-t(142))*0.001;
         tfour = t(t<limsup & t>liminf); vfour = v(t<limsup & t>liminf);
         
         M = 2 * ceil(length(vfour)/2);
@@ -64,7 +65,14 @@ function fourierOffset(path, nome_serie, sign, liminf, limsup)
         d1 = (double(A3)/A1)^(0.5);
 
         % Printo dati a file
-        fprintf(new_data,  '%s \n', [num2str(frequenze(ind1), 3) '    ' num2str(frequenze(ind3 + int32(7 * ind1/3)), 3) '    ' num2str(d1)]);
+        fprintf(new_data,  '%s \n', [num2str(frequenze(ind1)) '    ' num2str(frequenze(ind3 + int32(7 * ind1/3))) '    ' num2str(d1)]);
+
+        if i==sign(length(sign))
+            figure(2);
+            loglog(frequenze, amp, 'r-', 'LineWidth', 2); hold on; grid on;
+            xticks([10 100 1000 10000 100000]); xticklabels({'10^1', '10^2', '10^3', '10^4', '10^5'});
+            xlabel('log(f)'); ylabel('log(amp)'); title('Fourier Analysis');
+        end
     end
     fclose(new_data);  
 end
