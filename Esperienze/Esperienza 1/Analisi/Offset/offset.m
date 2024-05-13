b7645b9ca0a4ccac53cff757201c974c1e96435e
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-05-13 22:31:15 +0200

    Nuovo metodo: determinazione del centro del plasma per mezzo di una media pesata

--- Esperienze/Esperienza 1/Analisi/Offset/offset.m
+++ Esperienze/Esperienza 1/Analisi/Offset/offset.m
@@ -9,8 +9,8 @@ clc
 %---------------------------------------------------------%
 %    Offset in funzione del tempo di evoluzione libera    %
 %---------------------------------------------------------%
-base = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Ottici/serie';
-off_opt = zeros(1, 9);
+base = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Ottici/indMax/serie';
+opt_maxI = zeros(1, 9);
 % Calcolo quale sia l'offset medio per ciascuna delle prese dati - ottico
 for i=1:9
     % Apro il file, carico i vettori e riscalo opportunamente
@@ -18,11 +18,25 @@ for i=1:9
     N = 1; filescan = textscan(data,'%f %f %f','HeaderLines',N);
     x_centro = filescan {1,1}; y_centro = filescan {1,2}; offset_serie = filescan {1,3};
 
-    off_opt(i) = mean(offset_serie);
+    opt_maxI(i) = mean(offset_serie);
+end
+
+base = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Ottici/wmCharge/serie';
+opt_wmC = zeros(1, 9);
+% Calcolo quale sia l'offset medio per ciascuna delle prese dati - ottico
+for i=1:9
+    % Apro il file, carico i vettori e riscalo opportunamente
+    data = fopen([base num2str(i) '.txt'], 'rt');
+    N = 1; filescan = textscan(data,'%f %f %f','HeaderLines',N);
+    x_centro = filescan {1,1}; y_centro = filescan {1,2}; offset_serie = filescan {1,3};
+
+    opt_wmC(i) = mean(offset_serie);
 end
 
 figure (1);
 l_pixel = 90/838.0; t = [200, 300, 250, 150, 100, 125, 175, 225, 275];
-plot(t', off_opt' * l_pixel, 'r.', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
+plot(t', opt_maxI' * l_pixel, 'b+', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
+plot(t', opt_wmC' * l_pixel, 'r.', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
 title('Offset: dipendenza da evoluzione libera'); 
 xlabel('Tempo (ms)'); ylabel('Offset (mm)');
+legend('Optical: maxI', 'Optical: wmCharge')
--- Esperienze/Esperienza 1/Analisi/Offset/ottici_wmCharge.m
+++ Esperienze/Esperienza 1/Analisi/Offset/ottici_wmCharge.m
@@ -7,7 +7,7 @@
 % immagini e dei segnali scelti (saranno 9 contenitori, di cui 8 commentati
 % in fase di esecuzione -> dal primo all'ultimo ho corrispondenza con le
 % serie dalla prima all'ultima)
-sign = [2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 21, 22, 24, 29, 30, 31, 32, 33, 34, 35, 37, 39, 40, 41, 43, 45, 46, 47, 49];
+%sign = [2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 21, 22, 24, 29, 30, 31, 32, 33, 34, 35, 37, 39, 40, 41, 43, 45, 46, 47, 49];
 %sign = [2, 3, 4, 6, 8, 9, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48, 49, 50];
 %sign = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 42, 43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 56, 57, 58, 59, 60, 61, 62];
 %sign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
@@ -15,12 +15,12 @@ sign = [2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 21, 22, 24, 29, 30, 31,
 %sign = [1, 2, 3, 4, 6, 7, 8, 10];
 %sign = [1, 2, 4, 5, 6, 7, 8, 9, 10];
 %sign = [1, 3, 4, 5, 6, 8, 9, 10];
-%sign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+sign = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 % Riporto i nove possibili cammini che possono essere utilizzati durante
 % l'esecuzione di questo matlab script, in modo tale da non dover copiarlo
 % da terminale ogni volta
-path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series1_200ms/plasma';
+%path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series1_200ms/plasma';
 %path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series2_300ms/plasma';
 %path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series3_250ms/plasma';
 %path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series4_150ms/plasma';
@@ -28,24 +28,19 @@ path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/
 %path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series6_125ms/plasma';
 %path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series7_175ms/plasma';
 %path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series8_225ms/plasma';
-%path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series9_275ms/plasma';
+path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Dati/CAMimages/series9_275ms/plasma';
 
 
-% Definisco ora la procedura per analizzare i file di immagini: quello che
-% voglio fare è considerare solo le immagini che corrispondono a dei
-% segnali che abbiamo ritenuto ripetibili. Troviamo la riga e la colonna a
-% cui si trova il centro del plasma sommando inizialmente sulle ascisse e
-% poi sulle ordinate (una volta sulla matrice di partenza, mentre una sulla
-% trasposta per come funziona la funzione sum di matlab). I valori stimati
-% per ogni singola immagine vengono stampati a file: il file conterrà 3
-% colonne il cui significato sarà il seguente
-%           ---> coordinata x del centro del plasma
-%           ---> coordinata y del centro del plasma
-%           ---> valore del raggio
-function opticalOffset(path, sign, nome_serie)
+% Definisco ora la routine che mi consente di studiare quale sia l'offset
+% della colonna di plasma: sono interessato a trovare la posizione centrale
+% della regione a simmetria cilindrica occupata dal plasma stesso.
+% Lavoriamo effettuando una media pesata sull'intensità del singolo pixel
+% in modo da determinare l'ascissa e l'ordinata del centro del plasma
+% stesso.
+function opticalOffset_wmCharge(path, sign, nome_serie)
     % File per contenere i dati elaborati
-    new_data = fopen(['/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Ottici/' nome_serie],'w');
-    fprintf(new_data, '%s \n', 'Coordinata x       Coordinata y     Coordinata z');
+    new_data = fopen(['/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Ottici/wmCharge/' nome_serie],'w');
+    fprintf(new_data, '%s \n', 'Coordinata x       Coordinata y     Offset');
 
     % Ciclo solamente sulle immagini che corrispondono ad un buon segnale
     % sul settore S2
@@ -53,15 +48,19 @@ function opticalOffset(path, sign, nome_serie)
         % Leggo immagini
         M = imread([path sprintf('%03i', i) '.tif']); 
         M = double(M); [rowm, colm] = size(M);
-        % Trovo indici corretti e determino il raggio
-        appo = sum(M); [m1, col] = max(appo);
-        appo = sum(M'); [m2, row] = max(appo);
-        offset = sqrt((row - rowm/2 + 10)^2 + (col - colm/2)^2);
+        
+        % Faccio media pesata sull'intensità dei singoli pixel: devo in
+        % primo luogo creare una matrice delle ascisse e delle ordinate in
+        % modo tale da poter calcolare le coordinate del centro della
+        % colonna di plasma
+        [X, Y] = meshgrid(linspace(-colm/2, colm/2, colm), linspace(-rowm/2 + 10, rowm/2 + 10, rowm));
+        xm = X.*M; ym = Y.*M; col = sum(sum(xm))/sum(sum(M)); row = sum(sum(ym))/sum(sum(M));
 
+        offset = sqrt((row)^2 + (col)^2);
         fprintf(new_data,  '%s \n', [num2str(col) '    ' num2str(row) '    ' num2str(offset)]);
     end
     fclose(new_data);  
 end
 
 fileout = input('Nome file_out: \n');
-opticalOffset(path, sign, fileout);
+opticalOffset_wmCharge(path, sign, fileout);
