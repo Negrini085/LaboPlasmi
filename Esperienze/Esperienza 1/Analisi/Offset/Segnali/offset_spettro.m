ec2639ac14537f0351c1f3e3fa0a9965f5fdf14b
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-06-20 22:12:36 +0200

    Calcolo degli offset con metodi ottici ed FFT

--- Esperienze/Esperienza 1/Analisi/Offset/Segnali/offset_spettro.m
+++ Esperienze/Esperienza 1/Analisi/Offset/Segnali/offset_spettro.m
@@ -1,21 +1,56 @@
 %-------------------------------------------------------------------------%
-%               Grafico i diversi offset al passare del tempo             %
+%                   Determinazione degli offset con la FFT                %
 %-------------------------------------------------------------------------%
 
 close all
 clear all
 clc
 
+% Guadagno dell'amplificatore necessario per il corretto calcolo
+% dell'offset ---> I primi due sono fra 17000 e 18000, mentre la seconda
+% coppia di dati riguarda quelle frequenze poste fra 50000 Hz e 53000 Hz
+guad = [53.95, 53.1, 29.65, 28.15];
+
+% File di output
+path = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Segnali/Serie/serie';
+new_data = fopen('/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Segnali/offset.txt','w');
+fprintf(new_data, '%s \n','#_serie    Offset');
+
+% Lavoro sulle ampiezze dei modi precedentemente determinate
+for i=1:9
+    % Leggo segnali
+    data = fopen([path num2str(i) '.txt'], 'rt');
+    N = 1; filescan = textscan(data,'%f %f %f %f','HeaderLines',N);
+    omega1 = filescan {1,1}; a1 = filescan {1,2}; omega3 = filescan {1,3}; a3 = filescan {1,4}; 
+    disp(['Offset per serie ' num2str(i)])
+
+    offset = omega1;    
+    for j=1:length(omega1)
+        g1 = (omega1(j) - 17000) * (guad(2) - guad(1))/1000 + guad(1);
+        g3 = (omega3(j) - 50000) * (guad(4) - guad(3))/3000 + guad(3);
+        offset(j) = sqrt(g1/g3) * (a3(j)/a1(j));
+    end
+    % Printo dati a file
+    fprintf(new_data,  '%s \n', [ num2str(i) '    ' num2str(mean(offset))]);
+end
+fclose(new_data);  
+
+
 %-------------------------------------------------------------------------%
-%           Faccio grafico dell'offset tenendo conto del guadagno         %
+%             Determinazione frequenze prima e terza armonica             %
 %-------------------------------------------------------------------------%
+new_data = fopen('/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Segnali/frequenze.txt','w');
+fprintf(new_data, '%s \n','#_serie    Omega1    Omega3');
 
+disp(['Calcolo delle frequenze alle varie evoluzioni temporali'])
+% Lavoro con dati trovati con la FFT
+for i=1:9
+    % Leggo segnali
+    data = fopen([path num2str(i) '.txt'], 'rt');
+    N = 1; filescan = textscan(data,'%f %f %f %f','HeaderLines',N);
+    omega1 = filescan {1,1}; a1 = filescan {1,2}; omega3 = filescan {1,3}; a3 = filescan {1,4}; 
 
-
-figure (1);
-l_pixel = 90/838.0; t = [200, 300, 250, 150, 100, 125, 175, 225, 275];
-plot(t', opt_maxI' * l_pixel, 'b+', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
-plot(t', opt_wmC' * l_pixel, 'r.', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
-title('Offset: dipendenza da evoluzione libera'); 
-xlabel('Tempo (ms)'); ylabel('Offset (mm)');
-legend('Optical: maxI', 'Optical: wmCharge')
+    % Printo dati a file
+    fprintf(new_data,  '%s \n', [num2str(i) '    ' num2str(mean(omega1)) '    ' num2str(mean(omega3))]);
+end
+fclose(new_data);  
---
+++ Esperienze/Esperienza 1/Analisi/Offset/ottici_vs_FFT.m
@@ -0,0 +1,45 @@
+%-------------------------------------------------------------------------%
+%              Confronto calcolo offset (Ottici vs Segnali)               %
+%-------------------------------------------------------------------------%
+
+close all
+clear all
+clc
+
+t_ev = [200, 300, 250, 150, 100, 125, 175, 225, 275];
+
+% Leggo offset 
+path1 = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Ottici/offset.txt';
+path2 = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/Analisi/Offset/Segnali/offset.txt';
+
+% Leggo Ottici
+data1 = fopen(path1, 'rt');
+N = 1; filescan = textscan(data1,'%f %f ','HeaderLines',N);
+a = filescan {1,1}; off_ottici = filescan {1,2}; 
+fclose(data1);
+
+% Leggo Segnali
+data2 = fopen(path2, 'rt');
+N = 1; filescan = textscan(data1,'%f %f ','HeaderLines',N);
+b = filescan {1,1}; off_FFT = filescan {1,2}; 
+fclose(data2);
+
+
+figure (1);
+l_pixel = 90/838.0; 
+plot(t_ev', off_ottici * l_pixel, 'r.', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
+plot(t_ev', off_FFT * 1000, 'b.', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
+title('Offset: dipendenza da evoluzione libera'); 
+xlabel('Tempo (ms)'); ylabel('Offset (mm)');
+legend('Ottico', 'FFT')
+
+
+figure (2);
+l_pixel = 90/838.0; 
+fit_offset = regress(off_FFT * 1000, off_ottici * l_pixel);
+y_fit = fit_offset * off_ottici * l_pixel;
+plot(off_ottici * l_pixel, off_FFT * 1000, 'r.', 'MarkerFaceColor','auto', 'MarkerSize',20); hold on; grid on;
+plot(off_ottici * l_pixel, y_fit, 'r-', 'LineWidth', 2); hold on; grid on;
+title('Offset: Ottico vs FFT'); 
+xlabel('Analisi in frequenza (mm)'); ylabel('Analisi immagini (mm)');
+legend('Ottico vs FFT', 'Fit lineare')
