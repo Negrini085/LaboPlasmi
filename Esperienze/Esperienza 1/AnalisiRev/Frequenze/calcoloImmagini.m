95894d584ff0ede8e66711b5f3bd0db3044a9bda
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-07-25 13:31:11 +0200

    Calcolo frequenze a partire dalle immagini

--- Esperienze/Esperienza 1/AnalisiRev/Frequenze/calcoloImmagini.m
+++ Esperienze/Esperienza 1/AnalisiRev/Frequenze/calcoloImmagini.m
@@ -17,21 +17,28 @@ disp(['Frequenza modo uno di diocotron: ', num2str(nu_lin, 5)])
 
 % CORREZIONE PER OFFSET FINITO, DIMENSIONE RAGGIO FINITA E TEMPERATURA
 pathOff = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Offset/Risultati/Immagini/serie';
+pathOut = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Frequenze/Risultati/Immagini/serie';
 nu_r = 0; nu_rp = 0; nu_rpt = 0; off_pl = 0;
 for i=1:9
+    disp(['Analisi per serie ' num2str(i)])
     % Leggo Risultati
     path = [pathOff num2str(i) '.txt'];
     data = fopen(path, 'rt');
     N = 1; filescan = textscan(data,'%f %f %f','HeaderLines',N); offSerie = 45e-3/419 * filescan {1,3};
 
-    nuSerie_r = zeros(1, length(offSerie));
-    nuSerie_rp = zeros(1, length(offSerie));
-    nuSerie_rpt = zeros(1, length(offSerie));
+    pathr = [pathOut num2str(i) '_r.txt']; pathrp = [pathOut num2str(i) '_rp.txt']; pathrpt = [pathOut num2str(i) '_rpt.txt'];
+    nuSerie_r = zeros(1, length(offSerie)); nuSerie_rp = zeros(1, length(offSerie)); nuSerie_rpt = zeros(1, length(offSerie));
+
+    datar = fopen(pathr,'w'); fprintf(datar, '%s \n', '#        Nu1');
+    datarp = fopen(pathrp,'w'); fprintf(datarp, '%s \n', '#        Nu1');
+    datarpt = fopen(pathrpt,'w'); fprintf(datarpt, '%s \n', '#        Nu1');
     for j=1:length(offSerie)
-        nuSerie_r(j) = nu_lin * 1/(1-(offSerie(j)/Rw)^2);
-        nuSerie_rp(j) = nu_lin * (1 + ((1-2*(Rp/Rw)^2)/(1-(Rp/Rw)^2)^2) * (offSerie(j)/Rw)^2);
-        nuSerie_rpt(j) =  nuSerie_rp(j) * (1 + (2.405/2 * (0.25 + log(Rw/Rp) + (4*pi*eps0*T*Lp)/(Qp) - 0.671)*(Rw/Lp)));
+        nuSerie_r(j) = nu_lin * 1/(1-(offSerie(j)/Rw)^2); fprintf(datar, '%s \n', [num2str(i) '     ' num2str(nuSerie_r(j))]);
+        nuSerie_rp(j) = nu_lin * (1 + ((1-2*(Rp/Rw)^2)/(1-(Rp/Rw)^2)^2) * (offSerie(j)/Rw)^2); fprintf(datarp, '%s \n', [num2str(i) '     ' num2str(nuSerie_rp(j))]);
+        nuSerie_rpt(j) =  nuSerie_rp(j) * (1 + (2.405/2 * (0.25 + log(Rw/Rp) + (4*pi*eps0*T*Lp)/(Qp) - 0.671)*(Rw/Lp))); fprintf(datarpt, '%s \n', [num2str(i) '     ' num2str(nuSerie_rpt(j))]);
     end
+
+    fclose(datar); fclose(datarp); fclose(datarpt);
 
     if i == 1
         nu_r = nuSerie_r;
---
+++ Esperienze/Esperienza 1/AnalisiRev/Frequenze/confrontoFrequenze.m
@@ -0,0 +1,45 @@
+%-------------------------------------------------------------------------%
+%            CONFRONTO fra offset ottenuti con due metodologie            %
+%-------------------------------------------------------------------------%
+
+close all
+clear all
+clc
+
+baseIm = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Offset/Risultati/Immagini/serie';
+baseSign = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Offset/Risultati/Segnali/serie';
+
+% CICLO SUL NUMERO DI SERIE FATTE
+offIm = 0; offSign = 0;
+for i=1:9
+    % Leggo immagini
+    pathIm = [baseIm num2str(i) '.txt'];
+    data = fopen(pathIm, 'rt');
+    N = 1; filescan = textscan(data,'%f %f %f','HeaderLines',N);
+    offImSerie = filescan {1,3}; 
+
+    % Leggo segnali
+    pathSign = [baseSign num2str(i) '.txt'];
+    data = fopen(pathSign, 'rt');
+    N = 1; filescan = textscan(data,'%f %f','HeaderLines',N);
+    offSignSerie = filescan {1,2}; 
+    
+
+    % AGGIUNGO AI RISPETTIVI CONTENITORI
+    offIm = [offIm, offImSerie'];
+    offSign = [offSign, offSignSerie'];
+end
+
+% FACCIO IL FIT LINEARE
+offIm = offIm * 45/419; offSign = offSign * 1000;
+offIm = offIm(not (offIm < 0.2)); offSign = offSign(not (offSign < 0.2));
+myFit = polyfit(offIm, offSign, 1);
+
+
+figure;
+plot(offIm, offSign, 'r.', 'MarkerSize', 15, 'DisplayName', 'Valori'); hold on; grid on;
+plot(offIm, polyval(myFit, offIm), 'b-', 'LineWidth', 2, 'DisplayName', 'Fit'); hold on; grid on;
+xlabel('Offset ottici (mm)', 'FontSize', 12); ylabel('Offset DFT (mm)', 'FontSize', 12);
+legend('Location','southeast');
+
+disp(['Il coefficiente angolare della regressione lineare è: ' num2str(myFit(1))])
