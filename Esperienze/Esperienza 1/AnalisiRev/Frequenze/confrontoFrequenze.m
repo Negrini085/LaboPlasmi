3ed1deca94436a6d3d6815b35659419d69e5c5e7
Negrini085 <filippo.negrini@studenti.unimi.it>
2024-07-25 13:45:20 +0200

    Confronto fra frequenze

--- Esperienze/Esperienza 1/AnalisiRev/Frequenze/confrontoFrequenze.m
+++ Esperienze/Esperienza 1/AnalisiRev/Frequenze/confrontoFrequenze.m
@@ -6,40 +6,49 @@ close all
 clear all
 clc
 
-baseIm = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Offset/Risultati/Immagini/serie';
-baseSign = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Offset/Risultati/Segnali/serie';
+function confrFreq(baseSign, baseIm, tipo, aggiuntaIm)
+    disp(['Confronto fra frequenze a partire dai segnali e ' tipo]);
 
-% CICLO SUL NUMERO DI SERIE FATTE
-offIm = 0; offSign = 0;
-for i=1:9
-    % Leggo immagini
-    pathIm = [baseIm num2str(i) '.txt'];
-    data = fopen(pathIm, 'rt');
-    N = 1; filescan = textscan(data,'%f %f %f','HeaderLines',N);
-    offImSerie = filescan {1,3}; 
+    % CICLO SUL NUMERO DI SERIE FATTE
+    offIm = 0; offSign = 0;
+    for i=1:9
+        % Leggo immagini
+        pathIm = [baseIm num2str(i) aggiuntaIm];
+        data = fopen(pathIm, 'rt');
+        N = 1; filescan = textscan(data,'%f %f','HeaderLines',N);
+        offImSerie = filescan {1,2}; 
 
-    % Leggo segnali
-    pathSign = [baseSign num2str(i) '.txt'];
-    data = fopen(pathSign, 'rt');
-    N = 1; filescan = textscan(data,'%f %f','HeaderLines',N);
-    offSignSerie = filescan {1,2}; 
-    
+        % Leggo segnali
+        pathSign = [baseSign num2str(i) '.txt'];
+        data = fopen(pathSign, 'rt');
+        N = 1; filescan = textscan(data,'%f %f %f %f','HeaderLines',N);
+        offSignSerie = filescan {1,1};
 
-    % AGGIUNGO AI RISPETTIVI CONTENITORI
-    offIm = [offIm, offImSerie'];
-    offSign = [offSign, offSignSerie'];
+        % AGGIUNGO AI RISPETTIVI CONTENITORI
+        if i == 1
+            offIm = offImSerie';
+            offSign = offSignSerie';
+        else
+            offIm = [offIm, offImSerie'];
+            offSign = [offSign, offSignSerie'];
+        end
+    end
+
+    figure;
+    plot(offIm, offSign, 'r.', 'MarkerSize', 15, 'DisplayName', 'Valori'); hold on; grid on;
+    xlabel('Frequenze ottiche (Hz)', 'FontSize', 12); ylabel('Frequenze DFT (Hz)', 'FontSize', 12);
+    legend('Location','southeast');
 end
 
-% FACCIO IL FIT LINEARE
-offIm = offIm * 45/419; offSign = offSign * 1000;
-offIm = offIm(not (offIm < 0.2)); offSign = offSign(not (offSign < 0.2));
-myFit = polyfit(offIm, offSign, 1);
 
+pathSign = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Frequenze/Risultati/Segnali/serie';
+pathIm = '/home/filippo/Desktop/CODICINI/LABO_PLASMI/Esperienze/Esperienza 1/AnalisiRev/Frequenze/Risultati/Immagini/serie';
 
-figure;
-plot(offIm, offSign, 'r.', 'MarkerSize', 15, 'DisplayName', 'Valori'); hold on; grid on;
-plot(offIm, polyval(myFit, offIm), 'b-', 'LineWidth', 2, 'DisplayName', 'Fit'); hold on; grid on;
-xlabel('Offset ottici (mm)', 'FontSize', 12); ylabel('Offset DFT (mm)', 'FontSize', 12);
-legend('Location','southeast');
+% CASO CON CORREZIONE PER OFFSET FINITO
+confrFreq(pathSign, pathIm, 'correzione per offset finito sulle immagini', '_r.txt');
 
-disp(['Il coefficiente angolare della regressione lineare è: ' num2str(myFit(1))])
+% CASO CON CORREZIONE PER OFFSET E RAGGIO FINITI
+confrFreq(pathSign, pathIm, 'correzione per offset e raggio finiti sulle immagini', '_rp.txt');
+
+% CASO CON CORREZIONE PER OFFSET, RAGGIO E TEMPERATURA FINITE
+confrFreq(pathSign, pathIm, 'correzione per offset, raggio e temperatura finiti sulle immagini', '_rpt.txt');
